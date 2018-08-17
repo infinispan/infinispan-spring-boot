9b4481f32ce49c129001725c42f81125becd5512
Katia Aresti <karesti@redhat.com>
2018-08-17 17:40:01 +0200

    ISPN-9437 spring yaml - hotrod translation

--- infinispan-spring-boot-starter-embedded/src/test/java/test/org/infinispan/spring/starter/embedded/testconfiguration/InfinispanCacheTestConfiguration.java
+++ infinispan-spring-boot-starter-embedded/src/test/java/test/org/infinispan/spring/starter/embedded/testconfiguration/InfinispanCacheTestConfiguration.java
@@ -1,44 +1,48 @@
 package test.org.infinispan.spring.starter.embedded.testconfiguration;
 
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.StorageType;
 import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.eviction.EvictionStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import org.infinispan.eviction.EvictionType;
 import org.infinispan.spring.starter.embedded.InfinispanCacheConfigurer;
 import org.infinispan.spring.starter.embedded.InfinispanGlobalConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InfinispanCacheTestConfiguration {
 
-    public static final String TEST_CLUSTER = "TEST_CLUSTER";
-    public static final String TEST_CACHE_NAME = "test-simple-cache";
-    public static final String TEST_GLOBAL_JMX_DOMAIN = "test.infinispan";
+   public static final String TEST_CLUSTER = "TEST_CLUSTER";
+   public static final String TEST_CACHE_NAME = "test-simple-cache";
+   public static final String TEST_GLOBAL_JMX_DOMAIN = "test.infinispan";
 
-    @Bean
-    public InfinispanCacheConfigurer cacheConfigurer() {
-        return cacheManager -> {
-            final org.infinispan.configuration.cache.Configuration testCache =
-                    new ConfigurationBuilder().simpleCache(true)
-                                              .eviction().size(1000L).strategy(EvictionStrategy.LRU)
-                                              .jmxStatistics().enable()
-                                              .build();
+   @Bean
+   public InfinispanCacheConfigurer cacheConfigurer() {
+      return cacheManager -> {
+         final org.infinispan.configuration.cache.Configuration testCache =
+               new ConfigurationBuilder().simpleCache(true)
+                     .memory()
+                     .storageType(StorageType.OBJECT)
+                     .evictionType(EvictionType.COUNT)
+                     .evictionStrategy(EvictionStrategy.MANUAL)
+                     .jmxStatistics().enable()
+                     .build();
 
-            cacheManager.defineConfiguration(TEST_CACHE_NAME, testCache);
-        };
-    }
+         cacheManager.defineConfiguration(TEST_CACHE_NAME, testCache);
+      };
+   }
 
-    @Bean
-    public InfinispanGlobalConfigurer globalConfigurer() {
-        return () -> {
-            final GlobalConfiguration globalConfiguration = new GlobalConfigurationBuilder()
-                    .transport().clusterName(TEST_CLUSTER)
-                    .globalJmxStatistics().jmxDomain(TEST_GLOBAL_JMX_DOMAIN).enable()
-                    .build();
+   @Bean
+   public InfinispanGlobalConfigurer globalConfigurer() {
+      return () -> {
+         final GlobalConfiguration globalConfiguration = new GlobalConfigurationBuilder()
+               .transport().clusterName(TEST_CLUSTER)
+               .globalJmxStatistics().jmxDomain(TEST_GLOBAL_JMX_DOMAIN).enable()
+               .build();
 
-            return globalConfiguration;
-        };
-    }
+         return globalConfiguration;
+      };
+   }
 }
