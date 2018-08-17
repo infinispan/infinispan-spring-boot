95db1e20a7ddcb6a4ec57045a06c5644ed4c1c28
Katia Aresti <karesti@redhat.com>
2018-08-17 17:40:01 +0200

    ISPN-9437 spring yaml - hotrod translation

--- infinispan-spring-boot-starter-remote/src/main/java/org/infinispan/spring/starter/remote/InfinispanRemoteConfigurationProperties.java
+++ infinispan-spring-boot-starter-remote/src/main/java/org/infinispan/spring/starter/remote/InfinispanRemoteConfigurationProperties.java
@@ -1,89 +1,67 @@
 package org.infinispan.spring.starter.remote;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(value = "infinispan.remote", ignoreInvalidFields = true)
+public class InfinispanRemoteConfigurationProperties extends org.infinispan.client.hotrod.impl.ConfigurationProperties {
+   public static final String DEFAULT_CLIENT_PROPERTIES = "classpath:hotrod-client.properties";
 
-@ConfigurationProperties("infinispan.remote")
-public class InfinispanRemoteConfigurationProperties {
-    public static final String DEFAULT_CLIENT_PROPERTIES = "classpath:hotrod-client.properties";
+   /**
+    * Enable Infinispan support.
+    */
+   private boolean enabled = true;
 
-    /**
-     * Enable Infinispan support.
-     */
-    private boolean enabled = true;
+   /**
+    * Specifies a custom filename for Hot Rod client properties.
+    */
+   private String clientProperties = DEFAULT_CLIENT_PROPERTIES;
+   private final Map<String, String> saslProperties = new HashMap<>();
+   private final Map<String, String> cluster = new HashMap<>();
 
-    /**
-     * Specifies a custom filename for Hot Rod client properties.
-     */
-    private String clientProperties = DEFAULT_CLIENT_PROPERTIES;
+   public String getClientProperties() {
+      return clientProperties;
+   }
 
-    /**
-     * Defines a comma-separated list of Infinispan servers in this format:
-     * `host1[:port],host2[:port]`.
-     */
-    private String serverList;
+   public void setClientProperties(String clientProperties) {
+      this.clientProperties = clientProperties;
+   }
 
-    /**
-     * Sets a timeout value, in milliseconds, for socket connections.
-     */
-    private Integer socketTimeout;
+   public boolean isEnabled() {
+      return enabled;
+   }
 
-    /**
-     * Sets a timeout value for initializing connections with
-     * Infinispan servers.
-     */
-    private Integer connectTimeout;
+   public void setEnabled(boolean enabled) {
+      this.enabled = enabled;
+   }
 
-    /**
-     * Sets the maximum number of attempts to connect to Infinispan servers.
-     */
-    private Integer maxRetries;
+   public ConfigurationBuilder getConfigurationBuilder() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      Properties properties = this.getProperties();
+      saslProperties.forEach((k, v) -> this.getProperties().setProperty(SASL_PROPERTIES_PREFIX + "." + k, v));
+      cluster.forEach((k, v) -> this.getProperties().setProperty(CLUSTER_PROPERTIES_PREFIX + "." + k, v));
+      builder.withProperties(properties);
+      return builder;
+   }
 
-    public String getClientProperties() {
-        return clientProperties;
-    }
+   public Map<String, String> getSaslProperties() {
+      return saslProperties;
+   }
 
-    public void setClientProperties(String clientProperties) {
-        this.clientProperties = clientProperties;
-    }
+   public Map<String, String> getCluster() {
+      return cluster;
+   }
 
-    public boolean isEnabled() {
-        return enabled;
-    }
+   public void setSslProtocol(String sslProtocol) {
+      this.getProperties().setProperty(SSL_PROTOCOL, sslProtocol);
+   }
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
+   public void setAuthUserName(String authUserName) {
+      super.setAuthUsername(authUserName);
+   }
 
-    public String getServerList() {
-        return serverList;
-    }
-
-    public void setServerList(String serverList) {
-        this.serverList = serverList;
-    }
-
-    public Integer getSocketTimeout() {
-        return socketTimeout;
-    }
-
-    public void setSocketTimeout(Integer socketTimeout) {
-        this.socketTimeout = socketTimeout;
-    }
-
-    public Integer getConnectTimeout() {
-        return connectTimeout;
-    }
-
-    public void setConnectTimeout(Integer connectTimeout) {
-        this.connectTimeout = connectTimeout;
-    }
-
-    public Integer getMaxRetries() {
-        return maxRetries;
-    }
-
-    public void setMaxRetries(Integer maxRetries) {
-        this.maxRetries = maxRetries;
-    }
 }
