13b87eaa6b5a4d4bcd3731f75b661f8468d2ac80
Katia Aresti <karesti@redhat.com>
2018-11-08 12:02:10 +0100

    ISPN-9668 removes unused applicationcontext bean

--- infinispan-spring-boot-starter-embedded/src/main/java/org/infinispan/spring/starter/embedded/InfinispanEmbeddedAutoConfiguration.java
+++ infinispan-spring-boot-starter-embedded/src/main/java/org/infinispan/spring/starter/embedded/InfinispanEmbeddedAutoConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Conditional;
@@ -51,9 +50,6 @@ public class InfinispanEmbeddedAutoConfiguration {
 
    @Autowired(required = false)
    private List<InfinispanGlobalConfigurationCustomizer> globalConfigurationCustomizers = Collections.emptyList();
-
-   @Autowired
-   private ApplicationContext ctx;
 
    @Bean(destroyMethod = "stop")
    @Conditional(InfinispanEmbeddedCacheManagerChecker.class)
