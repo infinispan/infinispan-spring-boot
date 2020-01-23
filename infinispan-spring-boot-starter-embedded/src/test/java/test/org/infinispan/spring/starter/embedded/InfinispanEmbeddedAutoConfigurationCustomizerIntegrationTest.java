6264d4535ebee71e667f88f0229102c9035f02a0
Katia Aresti <karesti@redhat.com>
2020-01-23 15:41:39 +0100

    ISPN-11195 Start the manager when it's fully configured

--- infinispan-spring-boot-starter-embedded/src/test/java/test/org/infinispan/spring/starter/embedded/InfinispanEmbeddedAutoConfigurationCustomizerIntegrationTest.java
+++ infinispan-spring-boot-starter-embedded/src/test/java/test/org/infinispan/spring/starter/embedded/InfinispanEmbeddedAutoConfigurationCustomizerIntegrationTest.java
@@ -7,6 +7,10 @@ import java.util.UUID;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.eviction.EvictionType;
 import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachemanagerlistener.annotation.CacheStarted;
+import org.infinispan.notifications.cachemanagerlistener.event.CacheStartedEvent;
+import org.infinispan.spring.starter.embedded.InfinispanCacheConfigurer;
 import org.infinispan.spring.starter.embedded.InfinispanConfigurationCustomizer;
 import org.infinispan.spring.starter.embedded.InfinispanEmbeddedAutoConfiguration;
 import org.infinispan.spring.starter.embedded.InfinispanEmbeddedCacheManagerAutoConfiguration;
@@ -33,6 +37,8 @@ public class InfinispanEmbeddedAutoConfigurationCustomizerIntegrationTest {
    @Autowired
    EmbeddedCacheManager manager;
 
+   static CacheStartedEvent startedEvent;
+
    @Test
    public void testConfiguration() {
       assertThat(manager.getCacheManagerConfiguration().transport().clusterName()).isEqualTo(CLUSTER_NAME);
@@ -41,10 +47,20 @@ public class InfinispanEmbeddedAutoConfigurationCustomizerIntegrationTest {
       assertThat(manager.getCacheNames()).contains("small-cache");
       assertThat(manager.getCacheConfiguration("small-cache").memory().size()).isEqualTo(1000L);
       assertThat(manager.getCacheConfiguration("small-cache").memory().evictionType()).isEqualTo(EvictionType.COUNT);
+      assertThat(startedEvent).isNotNull();
    }
 
    @Configuration
    static class TestConfiguration {
+
+      @Listener
+      public class ExampleListener {
+         @CacheStarted
+         public void cacheStarted(CacheStartedEvent event) {
+            startedEvent = event;
+         }
+      }
+
       @Bean(name = "small-cache")
       public org.infinispan.configuration.cache.Configuration smallCache() {
          return new ConfigurationBuilder()
@@ -52,6 +68,11 @@ public class InfinispanEmbeddedAutoConfigurationCustomizerIntegrationTest {
                .memory().size(1000L)
                .memory().evictionType(EvictionType.COUNT)
                .build();
+      }
+
+      @Bean
+      public InfinispanCacheConfigurer cacheConfigurer() {
+         return manager -> manager.addListener(new ExampleListener());
       }
 
       @Bean
